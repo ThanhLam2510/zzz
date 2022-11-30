2389d1824aa2ceef0785536cd996cda919aeaf5a
hungcutephomaiqueconbocuoi <hung0868358175@gmail.com>
2022-12-01 01:48:35 +0700

    init

---
+++ configs/_local.js
@@ -0,0 +1,5 @@
+const config = {
+    mongoURI: "mongodb://doanhung:doanhung@cluster0-shard-00-00.jhi3o.mongodb.net:27017,cluster0-shard-00-01.jhi3o.mongodb.net:27017,cluster0-shard-00-02.jhi3o.mongodb.net:27017/polywork?replicaSet=atlas-u9vyql-shard-0&ssl=true&authSource=admin"
+  };
+module.exports = config;
+  
